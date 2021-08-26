6528871742dfe0fdb3020286fe370186182c5dfc
Irfianto <irfi@paras.id>
2021-08-26 21:36:59 +0700

    feat: nft_burn

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -308,6 +308,49 @@ impl Contract {
         return token_type_res.price.into();
     }
 
+    #[payable]
+    pub fn nft_burn(&mut self, token_id: TokenId) {
+        assert_one_yocto();
+
+        let owner_id = self.tokens.owner_by_id.get(&token_id).unwrap();
+        assert_eq!(
+            owner_id,
+            env::predecessor_account_id(),
+            "Token owner only"
+        );
+
+        if let Some(next_approval_id_by_id) = &mut self.tokens.next_approval_id_by_id {
+            next_approval_id_by_id.remove(&token_id);
+        }
+
+        if let Some(approvals_by_id) = &mut self.tokens.approvals_by_id {
+            approvals_by_id.remove(&token_id);
+        }
+
+        if let Some(tokens_per_owner) = &mut self.tokens.tokens_per_owner {
+            let mut token_ids = tokens_per_owner.get(&owner_id).unwrap();
+            token_ids.remove(&token_id);
+            tokens_per_owner.insert(&owner_id, &token_ids);
+        }
+
+        if let Some(token_metadata_by_id) = &mut self.tokens.token_metadata_by_id {
+            token_metadata_by_id.remove(&token_id);
+        }
+
+        self.tokens.owner_by_id.remove(&token_id);
+        env::log(
+            json!({
+                "type": "burn",
+                "params": {
+                    "token_id": token_id,
+                    "owner_id": owner_id,
+                }
+            })
+            .to_string()
+            .as_bytes(),
+        );
+    }
+
     // CUSTOM VIEWS
 
 	pub fn nft_get_type(&self, token_type: TokenTypeId) -> TokenTypeJson {
